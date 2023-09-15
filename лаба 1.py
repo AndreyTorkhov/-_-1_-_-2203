3bae7d2ba2428f07908a9acd051360f48f312f1f
unknown <askerbadah2004@gmail.com>
2023-09-15 13:38:37 +0300

    Asker

--- лаба 1.py
+++ лаба 1.py
@@ -4,7 +4,7 @@ a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
  
-d = b ** 2 - 4 * a * c
+d = b ** 2 - 4 * a * c+1
  
 if d > 0:
     x1 = (-b + math.sqrt(d)) / (2 * a)
